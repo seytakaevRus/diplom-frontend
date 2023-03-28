6dc09eb60cc583db83a0c0c0e7848d3e8c9aa7ea
isolya <osedysheva4@gmail.com>
2023-03-28 11:57:32 +0300

    хедер без ссылок на страницы

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,28 +1,56 @@
+import { LineStyle, Padding } from '@mui/icons-material';
 import MenuIcon from '@mui/icons-material/Menu'
-import { AppBar, Box, Button, IconButton, Link, MenuItem, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import { Container } from '@mui/system';
-import cl from './Header.module.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+const linkStyle = {
+  textDecoration: "none",
+};
+const MenuStyle = {
+  textDecoration: "none",
+  color:"white",
+  paddingRight:"50px",
+  fontSize:"bold",
+  fontWeight:"bold"
+}
 
 const Header = () => {
+ 
+
   return (
-   <div>
-      <AppBar position="fixed" color="primary" className={cl.AppBar}>
-        <Container fixed className={cl.container}>
-          <Toolbar>
-<IconButton edge="start" 
-color="inherit" area-aria-label='menu'>
-  <MenuIcon/>
-</IconButton>
-<Typography variant="h6" className={cl.logo}>EazyLearning.</Typography>
-<Box mr={3}> 
 
-  </Box>
+    <AppBar position="fixed" sx={{ background: 'black' }}>
+      <Toolbar>
+        <Link to="/" style={linkStyle}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: "bold",
+              padding: "10px",
+              margin:"10px 100px 10px 20px",
+              fontWeight: "bold",
+              position: "relative",
+              border: "5px solid #E98074",
+              borderRadius: "15px",
+              color: "#ffffff"
+            }}>
+            EazyLearning.
+          </Typography>
+        </Link>
+        <Box>
+        <Link to="/" style={MenuStyle}>Курсы</Link>
+        <Link to="/" style={MenuStyle}>Отзывы</Link>
+        <Link to="/" style={MenuStyle}>Вопросы</Link>
+        <Link to="/" style={MenuStyle}> Компьютер для занятий</Link>
 
-          </Toolbar>
-        </Container>
-      </AppBar>
-   </div>
+     
+        </Box>
+      </Toolbar>
+    </AppBar>
+
   );
-};
+      };
 
 export default Header;
