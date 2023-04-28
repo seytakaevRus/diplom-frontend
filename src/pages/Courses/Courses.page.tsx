1f6fc7baa13683d14e5ede3a2f017ad5eb242cba
isolya <osedysheva4@gmail.com>
2023-04-28 18:37:48 +0300

    filter

--- src/pages/Courses/Courses.page.tsx
+++ src/pages/Courses/Courses.page.tsx
@@ -10,29 +10,29 @@ import {
   Container,
 } from '@mui/material';
 import { useForm } from 'react-hook-form';
-
 import { useAppSelector } from '../../store/hooks';
 import { AudienceType } from '../../types/courses';
 import { Select } from '../../components/Select';
 import { audienceItems } from '../../constants/audience';
-
+import { childrenToReact } from 'react-markdown/lib/ast-to-react';
 interface CoursesInput {
   audience: AudienceType;
 }
-
 export const Courses = memo(() => {
   const { courseArray } = useAppSelector((state) => state.courses);
-
-  const { control } = useForm<CoursesInput>({
+  const { control, watch } = useForm<CoursesInput>({
     defaultValues: {
       audience: 'child',
     },
   });
-
   const linkStyle = {
     textDecoration: 'none',
     color: 'white',
   };
+  const watchAudience = watch('audience')
+  const filterCours = courseArray.filter((courseArray) => {
+    return courseArray.audience === watchAudience
+  });
 
   return (
     <Box sx={{ paddingTop: 20 }}>
@@ -46,13 +46,13 @@ export const Courses = memo(() => {
             fullWidth
           />
           <Box  display="grid" gridTemplateColumns="1fr 1fr" gap="20px">
-            {courseArray.map(({ id, title, cover }) => (
+            {filterCours.map(({ id, title, cover }) => (
               <Box key={id} display='grid' gap='10px' gridTemplateRows='1fr 40px 40px'>
                 <CardMedia
                   image={cover}
                   component="img"
                   sx={{
-                    maxWidth: '100%',
+                    maxWidth: '70%',
                     height: '100%'
                   }}
                 />
